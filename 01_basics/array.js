b1715b475c9cc724f1343fa2b3aff27faadf4c1a
Kshitij Patle <90553776+KshitijPatle@users.noreply.github.com>
2024-07-09 05:54:27 +0000

    arraychange

---
+++ 01_basics/array.js
@@ -0,0 +1,30 @@
+// const array =[1,2,3]
+// const array2 = [5,6,7]
+
+// // const array3 = array.concat(array2);
+// const array3 = [...array,...array2]
+
+// const array4 = [1,2,3,[4,5],[6],7,8,[9,10]]
+// const array5 = array4.flat(Infinity)
+// console.log(array5);
+
+// // array.push(4);
+
+// // console.log(array);
+
+// // array.pop()
+// // console.log(array);
+
+// // array.splice(0,2)
+// // console.log(array);
+
+// console.log(Array.from("Kshitij"));
+
+
+const a = 1
+const b = "Kshitij"
+const c= 34
+
+console.log(Array.of(a,b,c))
+
+
